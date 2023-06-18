12a15619d5dc91582c18c18d2eb20b0c58ac51b6
DungNaSa10 <dungnasa10@gmail.com>
2023-06-18 21:27:46 +0700

    Fix requirements

--- LAVIS-VQA/lavis/models/blip_models/blip.py
+++ LAVIS-VQA/lavis/models/blip_models/blip.py
@@ -21,7 +21,7 @@ class BlipBase(BaseModel):
     def __init__(self):
         super().__init__()
         transformers_version = version.parse(transformers.__version__)
-        assert transformers_version < version.parse("4.27"), "BLIP models are not compatible with transformers>=4.27, run pip install transformers==4.25 to downgrade"
+        assert transformers_version < version.parse("4.27"), "BLIP models are not compatible with transformers>=4.27, run pip install transformers==4.26 to downgrade"
         
     @classmethod
     def init_tokenizer(cls):
