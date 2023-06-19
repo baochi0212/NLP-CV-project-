216f583653222ec823cce2b65d54c3b4d4280d42
DungNaSa10 <dungnasa10@gmail.com>
2023-06-19 15:35:54 +0700

    Try distributed training

--- LAVIS-VQA/run_scripts/blip/train/train_textvqa.sh
+++ LAVIS-VQA/run_scripts/blip/train/train_textvqa.sh
@@ -1,2 +1,2 @@
-# python -m torch.distributed.run --nproc_per_node=16 train.py --cfg-path lavis/projects/blip/vqav2_ft.yaml
-python train.py --cfg-path lavis/projects/blip/train/textvqa_ft.yaml
+python -m torch.distributed.run --nproc_per_node=2 train.py --cfg-path lavis/projects/blip/train/textvqa_ft.yaml
+# python train.py --cfg-path lavis/projects/blip/train/textvqa_ft.yaml
