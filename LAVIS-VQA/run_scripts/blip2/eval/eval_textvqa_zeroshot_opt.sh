9ae5c3971e78838cbf58e676ee595fc905087be2
DungNaSa10 <dungnasa10@gmail.com>
2023-06-24 22:39:46 +0700

    Fix something

--- LAVIS-VQA/run_scripts/blip2/eval/eval_textvqa_zeroshot_opt.sh
+++ LAVIS-VQA/run_scripts/blip2/eval/eval_textvqa_zeroshot_opt.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.run --nproc_per_node=3 evaluate.py --cfg-path lavis/projects/blip2/eval/textvqa_zeroshot_opt.yaml
+python -m torch.distributed.run --nproc_per_node=2 evaluate.py --cfg-path lavis/projects/blip2/eval/textvqa_zeroshot_opt.yaml
