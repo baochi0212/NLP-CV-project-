350e77f87e4b3582ae38fd0f86e64be227619ea2
baochi0212 <chideptraiak29@gmail.com>
2023-06-24 10:35:18 +0000

    ..

--- LAVIS-VQA/run_scripts/blip/train/train_textvqa.sh
+++ LAVIS-VQA/run_scripts/blip/train/train_textvqa.sh
@@ -1,2 +1,2 @@
-python -m torch.distributed.run --nproc_per_node=2 train.py --cfg-path lavis/projects/blip/train/textvqa_ft.yaml
+python -m torch.distributed.run --nproc_per_node=3 train.py --cfg-path lavis/projects/blip/train/textvqa_ft.yaml
 # python train.py --cfg-path lavis/projects/blip/train/textvqa_ft.yaml
